20339954a8384d6f44a9ff2b93cdfec0275bddb0
DanEm255 <danielemery255@gmail.com>
2021-11-20 09:14:20 +0000

    build header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.scss";
+import Header from "./components/Header/header.component";
 import HomePage from "./pages/Homepage/homepage.component";
 import Hats from "./pages/Hats/hats.component";
 import Shop from "./pages/Shop/shop.component";
@@ -7,6 +8,7 @@ import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<Shop />} />
