096246bb4d2e558126b7343d42b64d7e420c6f0e
DanEm255 <danielemery255@gmail.com>
2022-01-07 09:00:15 +0000

    Include reselect library and add selectors to mapStateToProps

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,16 @@
+import { createSelector } from "reselect";
+
+const selectCart = (state) => state.cart;
+
+export const selectCartItems = createSelector(
+  [selectCart],
+  (cart) => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  (cartItems) =>
+    cartItems.reduce((previousValue, item) => {
+      return previousValue + item.quantity;
+    }, 0)
+);
